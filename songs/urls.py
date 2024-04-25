31d24c5639bad5a1103d6903fa4224a34696bb6e
ratchek <git@ratchek.com>
2024-04-25 09:52:10 -0400

    Add a way to see songs in a group

--- songs/urls.py
+++ songs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SongGroupView
+from .views import SongGroupView, GroupListView
 
 urlpatterns = [
-    path('', SongGroupView.as_view(), name='base'),  # add this line
+    path('song-group/<int:pk>', SongGroupView.as_view(), name='display_group_songs'),  # Display the songs in a group
+    path("", GroupListView.as_view(), name="index"),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Song
+from django.views import generic
+from .models import SongGroup
 
 class SongGroupView(View):
-    def get(self, request, *args, **kwargs):
-        songs = Song.objects.all()  # fetch all songs from the database
-        return render(request, 'songs/index.html', {'songs': songs})  # pass the songs to the template
+    def get(self, request, pk,  *args, **kwargs):
+        songs = SongGroup.objects.get(pk=pk).songs.all() # fetch all songs from the database
+        print(SongGroup.objects.get(pk=pk).songs.all())
+        return render(request, 'songs/display_group_songs.html', {'songs': songs})  # pass the songs to the template
+
+class GroupListView(generic.ListView):
+    model = SongGroup
+    template_name = 'songs/index.html'
+    context_object_name = 'groups'
+    ordering = ['name']
