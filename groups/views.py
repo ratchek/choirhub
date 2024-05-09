1009342311e3705609613efbe62b09e62d9d4e65
ratchek <git@ratchek.com>
2024-05-09 18:14:17 -0400

    Rework group template names

--- groups/views.py
+++ groups/views.py
@@ -3,11 +3,11 @@ from .models import SongGroup
 
 class GroupView(generic.DetailView):
     model = SongGroup
-    template_name = 'groups/display_group.html'
-    context_object_name = 'song_group'
+    template_name = 'groups/group.html'
+    context_object_name = 'group'
 
 class GroupListView(generic.ListView):
     model = SongGroup
-    template_name = 'groups/index.html'
+    template_name = 'groups/groups.html'
     context_object_name = 'groups'
     ordering = ['name']
