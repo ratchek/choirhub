ca9af8fdd0ce82e9bda771392040446db49923ed
ratchek <git@ratchek.com>
2024-05-09 18:44:21 -0400

    Fix slash in urls

--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import EventView, EventListView
 
 urlpatterns = [
-    path('/<int:pk>', EventView.as_view(), name='event-detail'),
-    path("/", EventListView.as_view(), name="event-list"),
+    path('<int:pk>', EventView.as_view(), name='event-detail'),
+    path("", EventListView.as_view(), name="event-list"),
 ]
--- groups/urls.py
+++ groups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import GroupView, GroupListView
 
 urlpatterns = [
-    path('group/<int:pk>', GroupView.as_view(), name='group-detail'),  # Display the songs in a group
+    path('group/<int:pk>', GroupView.as_view(), name='group-detail'),
     path("", GroupListView.as_view(), name="index"),
 ]
