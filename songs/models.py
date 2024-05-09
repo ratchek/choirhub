60184506532f2c5287de8bab7637ecb38184e0cf
ratchek <git@ratchek.com>
2024-05-09 12:11:55 -0400

    Create image files of uploaded scores

--- songs/models.py
+++ songs/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.conf import settings
 from django.core.validators import FileExtensionValidator
 
+# used for converting pdf to image
+from pdf2image import convert_from_bytes
+from django.core.files import File
+from io import BytesIO
 
 class Song(models.Model):
     title = models.CharField(max_length=255)
@@ -23,10 +27,16 @@ class SongScore(models.Model):
     # TODO add user folders once user auth is implemented
     # TODO add a custom validator to ensure that the file is a PDF (not just using extensions)
     file = models.FileField(upload_to=song_directory_path, validators=[FileExtensionValidator( ['pdf'] ) ])
-
+    img = models.ImageField(upload_to=song_directory_path, blank=True)  # image representation of the score
 
     def __str__(self):
         return f"{self.song.title} - score"
+
+    # override the save method to create an image from the uploaded PDF
+    def save(self, *args, **kwargs):
+        self.img = create_img_from_pdf(self.file)  # create an image from the uploaded PDF file
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+
 
 class SongRecording(models.Model):
     song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='recordings')
@@ -52,3 +62,12 @@ class SongGroup(models.Model):
 
     def __str__(self):
         return self.name
+
+### Helper functions ###
+def create_img_from_pdf(pdf):
+    """Creates an image of the first page of the pdf"""
+    images = convert_from_bytes(pdf.read())  # holds all the pages as an array of Pil images
+    img_io = BytesIO()  # needed as an intemediary to save the file from Pillow library
+    images[0].save(img_io, "JPEG", quality=85)  # save image to BytesIO object
+    img = File(img_io, name=pdf.name[:-4] + ".jpg")  # create a django friendly File object
+    return img
