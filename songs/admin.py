92681d03acadde7d68b516de771606f425266000
ratchek <git@ratchek.com>
2024-05-10 10:20:24 -0400

    Add inline song recordings

--- songs/admin.py
+++ songs/admin.py
@@ -5,10 +5,13 @@ class SongScoreInline(admin.TabularInline):
     model = SongScore
     fields = ['file',]
     extra = 1
+class SongRecordingInline(admin.TabularInline):
+    model = SongRecording
+    fields = ['voice_group', 'file',]
+    extra = 1
 
 class SongAdmin(admin.ModelAdmin):
-    inlines = [SongScoreInline]
-
+    inlines = [SongScoreInline, SongRecordingInline]
 # Register your models here.
 admin.site.register(Song, SongAdmin)
 admin.site.register(SongScore)
