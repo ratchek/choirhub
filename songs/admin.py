69d1fde606bc6e86e57a60829683775ce62eb8a7
ratchek <git@ratchek.com>
2024-05-10 10:17:47 -0400

    Add inline songscores

--- songs/admin.py
+++ songs/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Song, SongScore, SongRecording, VoiceGroup
 
+class SongScoreInline(admin.TabularInline):
+    model = SongScore
+    fields = ['file',]
+    extra = 1
+
+class SongAdmin(admin.ModelAdmin):
+    inlines = [SongScoreInline]
+
 # Register your models here.
-admin.site.register(Song)
+admin.site.register(Song, SongAdmin)
 admin.site.register(SongScore)
 admin.site.register(SongRecording)
 admin.site.register(VoiceGroup)
