321426466cf647d604dc38186c03f6d745b43dee
ratchek <git@ratchek.com>
2025-04-02 19:04:37 -0400

    Limit number of events in sidenav to 5

--- events/context_processors.py
+++ events/context_processors.py
@@ -2,6 +2,7 @@ from .models import Event
 # Make sure that the events are available in all views
 # needed for navigation
 def add_events_to_all_views(request):
+    no_of_events_to_display = 5
     return {
-        'nav_events': Event.objects.all().order_by('date').reverse()
+        'nav_events': Event.objects.all().order_by('date').reverse()[ : no_of_events_to_display]
     }
